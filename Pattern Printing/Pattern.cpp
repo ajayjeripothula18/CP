e031d50c409ed340b4db0e8ac516063629e21216
Ajay Jeripothula <84000025+ajayjeripothula18@users.noreply.github.com>
2022-03-23 22:24:02 +0530

    Update Pattern.cpp

--- Pattern Printing/Pattern.cpp
+++ Pattern Printing/Pattern.cpp
@@ -3,15 +3,19 @@ using namespace std;
 
 int main()
 {
-    cout<<"Pattern one";
-    cout<<endl;
-
+    cout<<"Pattern one"<<endl;
+  
     for(int i=0;i<=5;i++)
     {
         for(int j=0;j<=5;j++)
         {
             if(i>=j)
+            {
               cout<<" * ";
+            }
+            else
+                cout<<"   ";
+             
         }
         cout<<endl;
     }
