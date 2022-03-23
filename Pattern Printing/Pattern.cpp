9c460b023506248ca785a463a25aeb083705dc36
Ajay Jeripothula <84000025+ajayjeripothula18@users.noreply.github.com>
2022-03-23 22:16:09 +0530

    Create Pattern.cpp

---
+++ Pattern Printing/Pattern.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<"Pattern one";
+    cout<<endl;
+
+    for(int i=0;i<=5;i++)
+    {
+        for(int j=0;j<=5;j++)
+        {
+            if(i>j)
+              cout<<" * ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
